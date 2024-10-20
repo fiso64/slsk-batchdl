de977a01587f1b2f9191b4affc86d820053641f9
fiso64 <64778819+fiso64@users.noreply.github.com>
2024-10-20 16:36:32 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -8,11 +8,11 @@ mkdir -p slsk-batchdl/bin/zips
 # For macOS ARM64
 dotnet publish -c Release -r osx-arm64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
 rm -f slsk-batchdl/bin/Release/net6.0/osx-arm64/publish/*.pdb
-rm -f slsk-batchdl/bin/zips/slsk-batchdl_osx-arm64.zip
-zip -r slsk-batchdl/bin/zips/slsk-batchdl_osx-arm64.zip slsk-batchdl/bin/Release/net6.0/osx-arm64/publish
+rm -f slsk-batchdl/bin/zips/sldl_osx-arm64.zip
+zip -r slsk-batchdl/bin/zips/sldl_osx-arm64.zip slsk-batchdl/bin/Release/net6.0/osx-arm64/publish
 
 # For macOS x64
 # dotnet publish -c Release -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
 # rm -f slsk-batchdl/bin/Release/net6.0/osx-x64/publish/*.pdb
-# rm -f slsk-batchdl/bin/zips/slsk-batchdl_osx-x64.zip
-# zip -r slsk-batchdl/bin/zips/slsk-batchdl_osx-x64.zip slsk-batchdl/bin/Release/net6.0/osx-x64/publish
+# rm -f slsk-batchdl/bin/zips/sldl_osx-x64.zip
+# zip -r slsk-batchdl/bin/zips/sldl_osx-x64.zip slsk-batchdl/bin/Release/net6.0/osx-x64/publish
